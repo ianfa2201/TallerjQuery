b5f5e2c0714e155959654fab570b9444190a949b
ianfa2201 <ianf.blancol@konradlorenz.edu.co>
2021-05-27 22:00:20 -0500

    Se agrega el proyecto al repositorio GitHub

---
+++ js/funcionesBotones.js
@@ -0,0 +1,35 @@
+$(function() {
+    $("#show").click(function() {
+        $("#contenido").show();
+    });
+});
+
+$(function() {
+    $("#hide").click(function() {
+        $("#contenido").hide();
+    });
+});
+
+$(function() {
+    $("#fadeIn").click(function() {
+        $("#contenido").fadeIn();
+    });
+});
+
+$(function() {
+    $("#fadeOut").click(function() {
+        $("#contenido").fadeOut();
+    });
+});
+
+$(function() {
+    $("#slideUp").click(function() {
+        $("#contenido").slideUp();
+    });
+});
+
+$(function() {
+    $("#slideDown").click(function() {
+        $("#contenido").slideDown();
+    });
+});
